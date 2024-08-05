acfcc8a9088b69040c4ec400d2bae82261c5e39a
kwright73 <kayceewright@gmail.com>
2024-08-05 11:15:27 -0400

    Re-commit deliverable two

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let holes = prompt (`Hi ${memberName}, would you like to play 3 or 6 holes?`);
 console.log(`number of holes: ${holes}`);
 let gameStart = 3 * holes;
 let score = 0;
-for (let i = 0; i < holes; i++) {
-    let putt = Number (prompt("How many putts for holes 1? (par is 3)"));
+for (let i = 1; i <= holes; i++) {
+    let putt = Number (prompt(`How many putts for hole ${i}? (par is 3`));
     score += putt;
 }
 console.log(`Total number of putts: ${score}`);
 let par = gameStart - score;
 
 if (par > 0) {
-    console.log(`Great job, ${memberName}! Your total par was: ${par}.`);
+    console.log(`Nice try, ${memberName}! Your total par was: ${par}.`);
 }
 else if (par < 0) {
-    console.log(`Nice try, ${memberName}... Your total par was: ${par}.`);
+    console.log(`Great job, ${memberName}... Your total par was: ${par}.`);
 }
 else {
     console.log(`Good game, ${memberName}. Your total par was: ${par}.`);
